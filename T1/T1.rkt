e3be1a2c0f587cdc4cab3c2efc14590986a1debc
Joel Riquelme <joel.riquelme@ug.uchile.cl>
2024-08-22 23:26:14 -0400

    part a-d

---
+++ T1/T1.rkt
@@ -0,0 +1,76 @@
+#lang play
+(require math/flonum)
+
+#|
+Complete sus datos personales:
+NOMBRE Y APELLIDO: Joel Riquelme
+RUT: 20.499.444-7
+|#
+
+;; Parte a)
+
+#|
+<CFraction> ::= (simple <value>)
+              | (compound <value> <value> <CFraction>)
+
+<value> ::= Integer
+
+Type CFraction represent a finit Continued Fraction.
+|#
+(deftype CFraction
+  (simple a0)
+  (compound a0 b0 a1))
+
+;; Parte b)
+;; eval :: CFraction -> Rational
+;; Function that returns the eval CFraction in to Rational.
+(define (eval cf)
+  (match cf
+    [(simple a0) a0]
+    [(compound a0 b0 a1) (+ a0 (/ b0 (eval a1)))]))
+
+;; Parte c)
+;; degree ::  CFraction -> Integer
+;; Funtion rhat returns the degree of a CFRaction.
+(define (degree cf)
+  (match cf
+    [(simple _) 0]
+    [(compound _ _ a1) (+ 1 (degree a1))]))
+
+
+;; Parte d)
+;; fold-cfraction :: (Integer -> A) (Integer Integer A -> A) -> (CFraction -> A)
+;; Fold over CFrations.
+(define (fold-cfraction f g)
+  (λ (cf)
+    (match cf
+      [(simple a0) (f a0)]
+      [(compound a0 b0 a1) (g a0 b0 (fold-cfraction f g a1))])))
+                   
+;; Parte e)
+;; eval2 :: CFraction -> Rational
+;; Function that returns the eval CFraction in to Rational.
+(define eval2
+  (fold-cfraction identity +))
+
+;; degree2 ::  CFraction -> Integer
+;; Funtion rhat returns the degree of a CFRaction.
+(define degree2
+  (fold-cfraction (λ (_) 0) (λ (_) (+ 1))))
+
+;; Parte f)
+;; mysterious-cf :: Integer -> CFraction
+
+
+;; Parte g)
+;; from-to :: Integer Integer -> ListOf Integer
+
+
+;; mysterious-list :: Integer -> ListOf Float
+
+
+;; A que numero tiende (mysterious-cf k) cuando k tiende a infinito?
+
+
+;; Parte h)
+;; rac-to-cf :: Rational -> CFraction
---
+++ T1/test.rkt
@@ -0,0 +1,97 @@
+#lang play
+(require "T1.rkt")
+
+(print-only-errors #t)
+
+;; Part a)
+
+;; simple constructor
+(define a (simple 1))
+
+(test (simple? a) #t)
+(test (CFraction? a) #t)
+
+;; compound constructor
+(define cf (compound 12 1 (simple 4)))
+(define cf1 (compound 3 1 (compound 4 1 (compound 12 1 (simple 4)))))
+
+(test (compound? cf) #t)
+(test (CFraction? cf) #t)
+
+(test (compound? cf1) #t)
+(test (CFraction? cf1) #t)
+
+
+;; Part b)
+
+;; simple eval
+(define cf2 (simple 3))
+(test (eval cf2) 3)
+
+;; compound eval (int result)
+(define cf3 (compound 3 2 (simple 2)))
+(test (eval cf3) 4)
+
+;; compound eval (double result)
+(define cf4 (compound 1 1 (simple 2)))
+(test (eval cf4) 1.5)
+
+;; more complex case (int result)
+(define cf5 (compound 3 2 (compound 1 2 (simple 2))))
+(test (eval cf5) 4)
+
+;; more complex case (double result)
+(define cf6 (compound 1 1 (compound 0 2 (simple 1))))
+(test (eval cf6) 1.5)
+
+
+;; Part c)
+
+;; simple degree
+(define cf7 (simple 3))
+(test (degree cf7) 0)
+
+;; compound degree
+(define cf8 (compound 3 2 (simple 2)))
+(test (degree cf8) 1)
+
+;; more complex case
+(define cf9 (compound 3 1 (compound 4 1 (compound 12 1 (simple 4)))))
+(test (degree cf9) 3)
+
+
+;; Part d)
+
+;;
+
+;; Part e)
+
+;; simple eval
+(define cf10 (simple 3))
+(test (eval2 cf10) 3)
+
+;; compound eval (int result)
+(define cf11 (compound 3 2 (simple 2)))
+(test (eval2 cf11) 4)
+
+;; compound eval (double result)
+(define cf12 (compound 1 1 (simple 2)))
+(test (eval2 cf12) 1.5)
+
+;; more complex case (int result)
+(define cf13 (compound 3 2 (compound 1 2 (simple 2))))
+(test (eval2 cf13) 4)
+
+;; more complex case (double result)
+(define cf14 (compound 1 1 (compound 0 2 (simple 1))))
+(test (eval2 cf14) 1.5)
+
+
+
+
+
+
+
+
+
+
